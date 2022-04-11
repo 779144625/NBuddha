6747569282fa78035dd94369910bbefa255f6038
PiKaChu <15995518218@163.com>
2022-04-11 16:21:27 +0800

    上次push有问题 回退一下

--- NBuddha/spiders/buddha.py
+++ NBuddha/spiders/buddha.py
@@ -31,88 +31,38 @@ class BuddhaSpider(scrapy.Spider):
                         meta={"type": object_type},
                         dont_filter=True  # 不要过滤相同的url
                     )
+            elif "page_info" in l:
+                # print('0'*100)
+                page_info = l["page_info"]
+                media_info = page_info["media_info"]
+                url = media_info["stream_url_hd"]  # 获得了视频的url
+                object_type = 'video'
+                yield scrapy.Request(
+                    url=url,
+                    callback=self.parse_obj,
+                    meta={"type": object_type},
+                    dont_filter=True  # 不要过滤相同的url
+                )
+            else:
+                # 考虑到C哥的社会属性，有时候会转发一些文字内容，不包含图片和gif的，所以如果找不到这两个属性在当前这一part，就切换到下一个属性好了
+                logging.warning("Cant find page_info or pic_infos")
+                continue
+        page = int(response.request.url.split('=')[2][0]) + 1
+        print(page)
+        yield scrapy.Request(
+            url=self.origin_url + f'{page}' + '&feature=0',
+            callback=self.parse,
+            meta={}
+        )
 
-<< << << < HEAD
-elif "page_info" in l:
-# print('0'*100)
-page_info = l["page_info"]
-== == == =
-elif "page_info" in pic:
-print('0' * 100)
-page_info = pic["page_info"]
->> >> >> > 8616
-d9428a94ec7ef7e05e252d50abed1f220168
-media_info = page_info["media_info"]
-url = media_info["stream_url_hd"]  # 获得了视频的url
-object_type = 'video'
-yield scrapy.Request(
-    url=url,
-    callback=self.parse_obj,
-    meta={"type": object_type},
-    dont_filter=True  # 不要过滤相同的url
-)
-else:
-<< << << < HEAD
-# 考虑到C哥的社会属性，有时候会转发一些文字内容，不包含图片和gif的，所以如果找不到这两个属性在当前这一part，就切换到下一个属性好了
-logging.warning("Cant find page_info or pic_infos")
-continue
-page = int(response.request.url.split('=')[2][0]) + 1
-print(page)
-yield scrapy.Request(
-    url=self.origin_url + f'{page}' + '&feature=0',
-    callback=self.parse,
-    meta={}
-)
-== == == =
-logging.DEBUG("Cant find page_info or pic_infos")
-exit(1)
-page += 1
-# yield scrapy.Request(
-#     url=self.origin_url + f'{page}' + '&feature=0',
-#     callback=self.parse,
-#     meta={"item": item}
-# )
->> >> >> > 8616
-d9428a94ec7ef7e05e252d50abed1f220168
-
-
-def parse_obj(self, response):
-    """
-        传递给pipelines进行下载保存
-    """
-    url = response.request.url
-
-<< << << < HEAD
-item = {
-    'url': url,
-    'type': response.meta['type'],
-    'body': response.body
-}
-yield item
-== == == =
-
-if not os.path.exists(path):
-    os.mkdir(path)
-obj_type = response.meta["type"]
-if obj_type == "video":
-    title = url.split('?')[0].split('/')[-1]
-    # print('0'*100)
-    try:
-        with open(path + '/' + title, 'wb') as video:
-            video.write(response.body)
-    except Exception as e:
-        print(e)
-        exit(1)
-elif obj_type == "pic":
-    title = url.split('/')[-1]
-    try:
-        with open(path + '/' + title, 'wb') as pic:
-            pic.write(response.body)
-    except Exception as e:
-        print(e)
-        exit(1)
-else:
-    logging.DEBUG('Type is wrong')
-    exit(1)
->> >> >> > 8616
-d9428a94ec7ef7e05e252d50abed1f220168
+    def parse_obj(self, response):
+        """
+            传递给pipelines进行下载保存
+        """
+        url = response.request.url
+        item = {
+            'url': url,
+            'type': response.meta['type'],
+            'body': response.body
+        }
+        yield item
